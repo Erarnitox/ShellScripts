ce96929b3e873b63ea54530aef8a8676f2747e29
Erarnitox <erarnitox@gmail.com>
2019-12-03 01:45:50 +0100

    Added searching for interesting files

--- test.sh
+++ test.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 #Display basic information about the system
+echo "SYSTEM INFO:"
+echo "--------------------------"
+echo "Basic Information:"
 echo "Date:"
 date
 echo "User Info:"
@@ -9,3 +12,10 @@ echo "Logged in users:"
 who
 echo "Hostname:"
 hostname
+echo "---------------------------"
+#Find Files with suid or caps
+echo "Interesting Programms:"
+echo "With SUID:"
+find / -perm -u=s -type f 2>/dev/null
+echo "With Caps:"
+getcap -r / 2>/dev/null
