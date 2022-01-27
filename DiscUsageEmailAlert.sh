73dc1647e866752e2c4b0e20dbb8d90e368b0af5
Erarnitox <49777103+Erarnitox@users.noreply.github.com>
2022-01-27 01:27:48 +0100

    Create DiscUsageEmailAlert.sh

---
+++ DiscUsageEmailAlert.sh
@@ -0,0 +1,11 @@
+#Taken from: https://www.tecmint.com/basic-shell-programming-part-ii/
+
+#!/bin/sh
+
+MAX=95
+EMAIL=USER@domain.com
+PART=sda1
+USE=`df -h |grep $PART | awk '{ print $5 }' | cut -d'%' -f1`
+if [ $USE -gt $MAX ]; then
+  echo "Percent used: $USE" | mail -s "Running out of disk space" $EMAIL
+fi
